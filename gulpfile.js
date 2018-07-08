fe7952baed2fff1456df58beb1c3994f647d2776
SleepwalkerPWD <37107643+SleepwalkerPWD@users.noreply.github.com>
2018-07-08 20:21:34 +0300

    3

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ gulp.task('server', ['less'], function() {
     });
     gulp.watch('app/**/*.html').on('change', browserSync.reload);
     gulp.watch('app/less**/*.less', ['less']);
+    gulp.watch('app/**/*.js').on('change', browserSync.reload);
 });
 
 gulp.task ('less', function() {
